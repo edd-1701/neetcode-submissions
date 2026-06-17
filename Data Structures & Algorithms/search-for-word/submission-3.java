25b4b0798cddf2cef0263d446e40ab322ba1bfe5
Edd <67846232+edd-1701@users.noreply.github.com>
2026-06-17 15:55:30 -0400

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.java
@@ -0,0 +1,52 @@
+class Solution {
+    private char[][] board;
+    private boolean[][] visited;
+    private String word;
+
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.visited = new boolean[board.length][board[0].length];
+        this.word = word;
+
+        final var firstChar = word.charAt(0);
+
+        for (var row = 0; row < board.length; row++) {
+            for (var col = 0; col < board[0].length; col++) {
+                if (board[row][col] == firstChar && dfs(row, col, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(int row, int col, int indexAt) {
+
+        if (indexAt == word.length()) {
+            return true;
+        }
+
+        if (!isValidCell(row, col) || visited[row][col] || board[row][col] != word.charAt(indexAt)) {
+            return false;
+        }
+
+
+
+        final var charToMatch = word.charAt(indexAt);
+
+        visited[row][col] = true;
+        final var up = dfs(row-1, col, indexAt+1);
+        final var right = dfs(row, col+1, indexAt+1);
+        final var down =  dfs(row+1, col, indexAt+1);
+        final var left = dfs(row, col-1, indexAt+1);
+
+        visited[row][col] = false;
+        return up || right || down || left;
+    }
+
+    private boolean isValidCell(int row, int col) {
+        return row >= 0 && row < board.length && col >= 0 && col < board[0].length;
+    }
+
+}
