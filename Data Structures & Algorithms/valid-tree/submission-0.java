2a49ad0be71f7d947604199b39c577b6b1b4fb7b
Edd <67846232+edd-1701@users.noreply.github.com>
2026-07-03 15:16:14 -0400

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+
+        if (edges.length > n - 1) {
+            return false;
+        }
+
+        final var adj = buildGraph(n, edges);
+
+        final var visited = new boolean[n];
+        if (hasCycle(0, -1, adj, visited)) {
+            return false;
+        }
+
+
+        for (var visit : visited) {
+            if (!visit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasCycle(int node, int parent, Map<Integer, List<Integer>> adj, boolean[] visited) {
+        if (visited[node]) {
+            return true;
+        }
+
+        visited[node] = true;
+        for (var nei : adj.get(node)) {
+            if (nei == parent) {
+                continue;
+            }
+
+            if (hasCycle(nei, node, adj, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    private Map<Integer, List<Integer>> buildGraph(int n, int[][] adj) {
+        final var result = new HashMap<Integer, List<Integer>>();
+
+        for (var i = 0; i < n; i++) {
+            result.put(i, new ArrayList<Integer>());
+        }
+
+        for (var edge : adj) {
+            result.get(edge[0]).add(edge[1]);
+            result.get(edge[1]).add(edge[0]);
+        }
+
+        return result;
+    }
+}
