9648273e3cc9c6a6a8853ee6fd6661227c952c7c
Edd <67846232+edd-1701@users.noreply.github.com>
2026-07-06 17:58:47 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,51 @@
+/*
+
+    Build an adjancecy list
+    birectional
+
+    foreach node:
+        if not visited, mark as a new component, then do a dfs to find all the connections
+*/
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        var count = 0;
+
+        final var adj = buildGraph(n, edges);
+        final var visited = new boolean[n];
+        for (var i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            count += 1;
+            dfs(i, visited, adj);
+        }
+
+        return count;
+
+    }
+
+    private void dfs(int node, boolean[] visited, Map<Integer, List<Integer>> graph) {
+        visited[node] = true;
+
+        for (var nei : graph.get(node)) {
+            if (!visited[nei]) {
+                dfs(nei, visited, graph);
+            }
+        }
+    }
+
+    private Map<Integer, List<Integer>> buildGraph(int n, int[][] edges) {
+        final var result = new HashMap<Integer, List<Integer>>();
+
+        for (var i = 0; i<n; i++) {
+            result.put(i, new ArrayList<Integer>());
+        }
+
+        for (var conn : edges) {
+            result.get(conn[0]).add(conn[1]);
+            result.get(conn[1]).add(conn[0]);
+        }
+        return result;
+    }
+}
