7fa599a0cc32817f85837cf6cefba741e0a9c88e
Edd <67846232+edd-1701@users.noreply.github.com>
2026-07-27 18:51:54 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    private final static Map<Integer, Integer> CACHE = new HashMap<>();
+
+    public boolean isHappy(int n) {
+        final var visited = new HashSet<Integer>();
+
+        while (visited.add(n)) {
+            n = sumOfSquaredDigits(n);
+
+            if (n == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int sumOfSquaredDigits(int num) {
+        if (CACHE.containsKey(num)) {
+            return CACHE.get(num);
+        }
+
+        var result = 0;
+
+        while (num > 0) {
+            final var digit = num % 10;
+            result += (digit * digit);
+            num /= 10;
+        }
+
+        CACHE.put(num, result);
+        return result;
+    }
+}
