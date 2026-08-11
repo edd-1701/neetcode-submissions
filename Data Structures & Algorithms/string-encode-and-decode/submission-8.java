6c1c06c62f97ab6836b0fe57be6226101c888eeb
Edd <67846232+edd-1701@users.noreply.github.com>
2026-08-11 19:08:07 -0400

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.java
@@ -0,0 +1,38 @@
+class Solution {
+    private final static String DELIMITER = "%%%%";
+    private final static String EMPTY = "@@@";
+
+    public String encode(List<String> strs) {
+        if (strs.isEmpty()) {
+            return "";
+        }
+        final var result = new StringBuilder();
+        var isFirst = true;
+        for (var str : strs) {
+            final var delimiter = isFirst ? "" : DELIMITER;
+            result.append(delimiter).append(str.isEmpty() ? EMPTY : str);
+
+            if (isFirst) {
+                isFirst = false;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public List<String> decode(String str) {
+        if (str.length() == 0) {
+            return List.of();
+        }
+
+        final var result = new ArrayList<String>();
+        
+        final var split = str.split(DELIMITER);
+
+        for (var chunk : split) {
+            result.add(chunk.equals(EMPTY) ? "" : chunk);
+        }
+
+        return result;
+    }
+}
