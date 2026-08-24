ad2727bbc049d8352b3f398ca08bc2b96e3259b0
Edd <67846232+edd-1701@users.noreply.github.com>
2026-08-24 15:52:17 -0400

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        final var charMap = new HashMap<Character, Integer>();
+
+        for (var i = 0; i < t.length(); i++) {
+            charMap.merge(t.charAt(i), 1, Integer::sum);
+        }
+
+        var minWindow = Integer.MAX_VALUE;
+        var resultIdxs = new int[2];
+
+        var left = 0;
+        var right = 0;
+        while (right < s.length()) {
+            final var currChar = s.charAt(right);
+
+            charMap.merge(currChar, -1, Integer::sum);
+
+            while (hasAll(charMap.values())) {
+                if (right - left + 1 < minWindow) {
+                    minWindow = right - left + 1;
+                    resultIdxs[0] = left;
+                    resultIdxs[1] = right;
+                }
+
+                charMap.merge(s.charAt(left), 1, Integer::sum);
+
+                left += 1;
+            }
+
+            right += 1;
+        }
+
+        return minWindow == Integer.MAX_VALUE ? "" : s.substring(resultIdxs[0], resultIdxs[1] + 1);
+    }
+
+    private static boolean hasAll(Collection<Integer> counts) {
+        for (var c : counts) {
+            if (c > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
