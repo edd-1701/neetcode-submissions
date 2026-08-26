be1201e61a1c5622eae84e86d42617bf53dd444a
Edd <67846232+edd-1701@users.noreply.github.com>
2026-08-26 19:18:26 -0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        var left = 0;
+        var right = nums.length - 1;
+
+        while (left <= right) {
+            final var middle = (right + left) / 2;
+
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+}
