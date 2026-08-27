155fe8cbfba474173ce55b7a248dbf0bacd5357a
Edd <67846232+edd-1701@users.noreply.github.com>
2026-08-27 18:08:23 -0400

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        final var ROWS = matrix.length;
+        final var COLS = matrix[0].length;
+
+        for (var row = 0; row < ROWS; row++) {
+            if (target > matrix[row][COLS-1]) {
+                continue;
+            }
+
+            var left = 0;
+            var right = COLS - 1;
+
+            while (left <= right) {
+                final var middleIdx = (left + right) / 2;
+
+                if (matrix[row][middleIdx] < target) {
+                    left = middleIdx + 1;
+                } else if (matrix[row][middleIdx] > target) {
+                    right = middleIdx - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+
+        return false;
+     }
+}
